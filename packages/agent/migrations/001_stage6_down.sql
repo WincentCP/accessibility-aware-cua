4474490558df4881c0760c744202799fe2b4a79d
Wincent Coleus Phan <wincentcoleusphan@gmail.com>
2026-08-25 13:26:36 +0700

    feat: implement Stage 6 durable agent state

---
+++ packages/agent/migrations/001_stage6_down.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+DROP TABLE IF EXISTS interventions;
+DROP TABLE IF EXISTS focus_handoffs;
+DROP TABLE IF EXISTS task_map_snapshots;
+DROP TABLE IF EXISTS verifications;
+DROP TABLE IF EXISTS agent_steps;
+DROP TABLE IF EXISTS task_runs;
+DROP TABLE IF EXISTS experiment_configs;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS sessions;
+DELETE FROM schema_migrations WHERE version = '001_stage6';
+DROP TABLE IF EXISTS schema_migrations;
+
+COMMIT;
